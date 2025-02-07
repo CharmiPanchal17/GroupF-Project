27db5ee3ba97a97783d0ce82d4a8fe2756b65a04
Anthony-B <byamugishanthony@gmail.com>
2025-02-07 12:35:40 +0300

    Added login and registration forms

--- aits/registration/urls.py
+++ aits/registration/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('register_student/', views.register_student, name='register_student'),
+    path('register_lecturer/', views.register_lecturer, name='register_lecturer'),
+    path('login_user/', views.login_user, name='login_user'),
 ]
--- aits/registration/views.py
+++ aits/registration/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def register_student(request):
+    return render(request, 'register_student.html')
+
+def register_lecturer(request):
+    return render(request, 'register_lecturer.html')
+
+def login_user(request):
+    return render(request, 'login_user.html')
