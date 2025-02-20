3067c04e8426df9f1713f6883c6c375dacc35d07
CharmiPanchal17 <charmipanchal17@gmail.com>
2025-02-20 12:43:43 +0300

    creating models

--- AITS/issues/models.py
+++ AITS/issues/models.py
@@ -17,4 +17,27 @@ class Issue(models.Model):
     def __str__(self):
         return self.title
 
- 
+class RegisteredStudent(models.Model):
+    YEAR_CHOICES = [
+        ('1', 'Year one'),
+        ('2', 'Year two'),
+        ('3', 'Year three'),
+        ('4', 'Year four'),
+    ]
+
+    COURSE_CHOICES = [
+        ('CS', 'Computer Science'),
+        ('LI', 'Library and Information Technology'),
+        ('SE', 'Software Engineering')
+    ]
+
+    name = models.CharField(max_length=100)
+    student_id = models.CharField(max_length=15, unique=True)
+    year = models.CharField(max_length=1, choices=YEAR_CHOICES, default='1')
+    course = models.CharField(max_length=2, choices= COURSE_CHOICES)
+    issue = models.TextField()
+    created_at = models.DateTimeField(auto_now_add= True)
+
+
+
+
