fe73ce38bef9ab620c4c8610da8b5a11b7dafd6a
CharmiPanchal17 <charmipanchal17@gmail.com>
2025-02-23 23:52:25 +0300

    registering models

--- AITS/issues/admin.py
+++ AITS/issues/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from .models import User, Student, Lecturer, Registrar, Issue, Notification
 
-from .models import Issue
-
+admin.site.register(User)
+admin.site.register(Student)
+admin.site.register(Lecturer)
+admin.site.register(Registrar)
 admin.site.register(Issue)
+admin.site.register(Notification)
