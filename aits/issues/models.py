ad06c7414f295cc40f7f991e30336f5f81034579
Anthony-B <byamugishanthony@gmail.com>
2025-03-05 13:02:35 +0300

    added the issue model

--- aits/issues/models.py
+++ aits/issues/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Issue(models.Model):
+    STATUS_CHOICES = (
+        ('open' , 'Open'),
+        ('pending' , 'Pending'),
+        ('resolved' , 'Resolved'),
+    )
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    submitted_by = models.ForeignKey(User,on_delete=models.CASCADE, related_name='issues_submitted')
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='issues_assigned', null =True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=25, default='open')
+
+    def __str__(self):
+        return self.title
+ 
