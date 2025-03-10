bf9e690affb5e25c0ec313783a67534b3f52f73c
CharmiPanchal17 <charmipanchal17@gmail.com>
2025-03-10 22:28:52 +0300

    editing serializers and endpoints

--- AITS/AITS/settings.py
+++ AITS/AITS/settings.py
@@ -123,4 +123,6 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+AUTH_USER_MODEL = 'issues.User'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- AITS/AITS/urls.py
+++ AITS/AITS/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # path('', include('registration.urls')),
     path('admin/', admin.site.urls),
-    # path('api/', include('issues.urls')),
     path('', include('issues.urls')),
 ]
