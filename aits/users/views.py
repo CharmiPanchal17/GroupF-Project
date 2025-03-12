02e7d596e378ba2df1370e01baaf9936facec82e
Anthony-B <byamugishanthony@gmail.com>
2025-03-13 02:01:31 +0300

    added registration views

--- aits/users/views.py
+++ aits/users/views.py
@@ -1,13 +1,37 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, permissions, generics
+from .serializers import StudentSerializer, LecturerSerializer,RegistrarSerializer, UserSerializer
 from .models import User
-from .serializers import UserSerializer
+
+class LecturerListSerializer(UserSerializer):
+    class Meta(UserSerializer.Meta):
+        fields = ['id', 'email', 'first_name', 'last_name', 'role', 'lecturer_reg_number']
 
 class LecturerViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    serializer_class = LecturerListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class RegisterStudentView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = StudentSerializer
+    permission_classes = [permissions.AllowAny]
+
+class RegisterLecturerView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = LecturerSerializer
+    permission_classes = [permissions.AllowAny]
+
+class RegisterRegistrarView(generics.CreateAPIView):  
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegistrarSerializer
+    permission_classes = [permissions.AllowAny] 
 
     def get_queryset(self):
-        if self.request.user.role =='registrar':
+        if self.request.user.role == 'registrar':
             return User.objects.filter(role='lecturer')
         return User.objects.none()
+
+
+    
