6c9a757d03431aaf6b57208d2e1bbbed5137e432
Anthony-B <byamugishanthony@gmail.com>
2025-03-13 02:03:47 +0300

    Added registration urls

--- aits/users/urls.py
+++ aits/users/urls.py
@@ -1,1 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import RegisterLecturerView, RegisterStudentView, RegisterRegistrarView, LecturerViewSet
+
+router = DefaultRouter()
+router.register('lecturers', LecturerViewSet, basename='lecturers')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('register/student/', RegisterStudentView.as_view(), name='register-student'),
+    path('register/lecturer/', RegisterLecturerView.as_view(), name='register-lecturer'),
+    path('register/registrar/', RegisterRegistrarView.as_view(), name='register-registrar'),
+]
