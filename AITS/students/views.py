b3367ed8c6af936c790eece1c9ad7e6df493ae23
priscilla199 <kobumanzipriscilla@gmail.com>
2025-03-19 17:57:23 +0300

    s

--- AITS/students/views.py
+++ AITS/students/views.py
@@ -5,6 +5,11 @@ from django.http import HttpResponse
 def studentdata(request):
     return HttpResponse("*******Student list with issues********")
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
 from rest_framework import viewsets
 from .models import Studententry
 from .serializers import Studententryserializer
@@ -15,3 +20,40 @@ class StudententryViewSet(viewsets.ModelViewSet):
     """
     queryset = Studententry.objects.all()
     serializer_class = Studententryserializer
+
+@api_view(['GET'])
+def get_user(request):
+    queryset = Studententry.objects.all()
+    serializer = Studententryserializer(queryset, many=True)
+    return Response(serializer.data )
+
+@api_view(['POST'])
+def create_user(request):
+        serializer=Studententryserializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data , status=status.HTTP_201_CREATED)
+        return Response(serializer.errors , status= status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def queryset_detail(request,pk):
+     try:
+          user =Studententry.objects.get(pk=pk)
+     except Studententry.DoesNotExist:
+          return Response(status=status.HTTP_404_NOT_FOUND)
+     
+
+     if request.method == 'GET':
+          serializer=Studententryserializer(user)
+          return Response(serializer.data)
+     
+     elif request.method == 'PUT':
+          serializer =Studententryserializer(user , data=request.data)
+          if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+          return Response(serializer.errors , status= status.HTTP_400_BAD_REQUEST)
+     
+     elif request.method == 'DELETE':
+          user.delete()
+          return Response(status=status.HTTP_204_NO_CONTENT)
