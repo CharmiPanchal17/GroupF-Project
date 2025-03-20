8900f3779d0dbd414bbc2464712929f7eda03180
priscilla199 <kobumanzipriscilla@gmail.com>
2025-03-20 13:19:21 +0300

    s

--- AITS/meineapp/admin.py
+++ AITS/meineapp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student,Notification,CustomUser,Issue,Assignment
 
 # Register your models here.
 admin.site.register(Student)
+admin.site.register(CustomUser)
+admin.site.register(Notification)
+admin.site.register(Issue)
+admin.site.register(Assignment)
