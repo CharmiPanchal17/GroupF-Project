936bfdafc9c9f210b719324841bc7235662980e1
priscilla199 <kobumanzipriscilla@gmail.com>
2025-03-20 14:29:52 +0300

    s

--- AITS/Issue/urls.py
+++ AITS/Issue/urls.py
@@ -8,3 +8,13 @@ router.register(r'issues', IssueViewSet)
 urlpatterns = [
     path('api/', include(router.urls)),
 ]
+
+from django.urls import path
+from .views import issue_list, issue_detail, assign_issue, user_notifications
+
+urlpatterns = [
+    path('issues/', issue_list, name='issue-list'),
+    path('issues/<int:pk>/', issue_detail, name='issue-detail'),
+    path('assign/', assign_issue, name='assign-issue'),
+    path('notifications/', user_notifications, name='user-notifications'),
+]
