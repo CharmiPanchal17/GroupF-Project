2335c0e13416cc6d95f12ef779cd683ef2571156
priscilla199 <kobumanzipriscilla@gmail.com>
2025-03-20 14:31:04 +0300

    s

--- AITS/Issue/admin.py
+++ AITS/Issue/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Notification,CustomUser,Issue,Assignment
+
 
 # Register your models here.
+admin.site.register(Issue)
+admin.site.register(CustomUser)
+admin.site.register(Notification)
+
+admin.site.register(Assignment)
