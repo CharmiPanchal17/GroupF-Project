3b37c892e239a87d9b17584ebdaf9a3989f2b02e
priscilla199 <kobumanzipriscilla@gmail.com>
2025-03-23 21:01:49 +0300

    organised work

--- AITS/issues/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Student, Lecturer, Registrar, Issue, Notification
-
-admin.site.register(User)
-admin.site.register(Student)
-admin.site.register(Lecturer)
-admin.site.register(Registrar)
-admin.site.register(Issue)
-admin.site.register(Notification)
