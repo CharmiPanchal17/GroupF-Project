ce11228127d0b0e6214d5c0b0e0a77d74b1b8bd0
Aaliyah-101 <aaliyahiga101@gmail.com>
2025-03-28 00:11:56 +0300

    Needs more features

--- src/StudentDashboard.js
+++ src/StudentDashboard.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import './StudentDashboard.css'
 import { useNavigate } from "react-router-dom"
 
 const StudentDashboard = ({ user }) =>
@@ -7,23 +8,37 @@ const StudentDashboard = ({ user }) =>
 //data for course units
         const [courseUnits, setCourseUnits] = useState([
             { name: "Statistics", status: "resolved"},
-            {name: "Computer literacy", status: "resolved"},
-            {name: "Programming", status: "pending"},
-            {name: "Robotics", status: "denied"}
+            { name: "Computer literacy", status: "resolved"},
+            { name: "Programming", status: "pending"},
+            { name: "Robotics", status: "denied"}
         ]);
+        console.log(courseUnits);
 
         return (
-            <div style={{ padding: "20px", textAlign: "centre" }}>
+            <div style={{ padding: "20px", textAlign: "left" }}>
                 <h1>Student Dashboard</h1>
-                <h2> Hi, {user.username}! </h2>
-                <p>Track your issueresolutins with AITS</p>
+                <h2> Hi, {user?.username || "Student"}! </h2>
+                <h3>Track your issue resolutions with AITS</h3>
+
+                <div className="course-units-container">
+                    <div className="course-unit-header">
+                        <div className="course-unit-name=header">Course unit</div>
+                        <div className="course-unit-status-header">Status</div>
+                    </div>
+                    <ul className="course-units-list">
+                        {courseUnits.map((unit, index) => (
+                            <li key={index} 
+                                className={`course-unit ${
+                                    unit.status === "resolved" ? "resolved" : unit.status === "pending" ? "pending" : "denied"}`}
+                                   >   
+                                   <strong>{unit.name}</strong> - {unit.status}
+                            </li>
+                        ))}
+                        </ul>
+                </div>
                 <button
-                onClick={() => navigate("/complaint")}
-                style={{ marginTop: "20px",
-                    padding: "10px 20px",
-                    fontSize: "16px", cursor: "pointer" }}
-                    >
-                        Submit an issue
+                  className="submit-issue-button"
+               onClick={() => navigate("/complaint")}>Submit an issue
                     </button>
             </div>
         );
