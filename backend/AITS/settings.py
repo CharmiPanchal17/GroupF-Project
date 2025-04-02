c7228c69ebf6e7bb25aa9d788bf2d3cfebbe56ed
CharmiPanchal17 <charmipanchal17@gmail.com>
2025-04-02 13:39:39 +0300

    adding corsheaders

--- backend/AITS/settings.py
+++ backend/AITS/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # Third-party apps
     'rest_framework_simplejwt',
     'rest_framework',
+    'corsheaders',
     #apps
     'base',
     
@@ -55,6 +56,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    
 ]
 
 ROOT_URLCONF = 'AITS.urls'
