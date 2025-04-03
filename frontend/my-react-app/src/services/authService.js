0d8ac30a7e1de96cf161b5afde9e118977e29d50
juliakiconco <juliakiconco@gmail.com>
2025-04-03 16:22:41 +0300

    services

--- frontend/my-react-app/src/services/authService.js
+++ frontend/my-react-app/src/services/authService.js
@@ -1,16 +1,28 @@
 // src/services/authService.js
-
 import axios from 'axios';
+import toast from "react-hot-toast";
 
-const API_URL = 'http://localhost:8000/api/token/';
+const API_URL = `${process.env.REACT_APP_API_URL}/token/`;
 
-const login = async (username, password) => {
-    const response = await axios.post(API_URL, { username, password });
-    if (response.data) {
-        localStorage.setItem('token', response.data.access);
-        localStorage.setItem('refresh_token', response.data.refresh);
+
+const login = async (data) => {
+
+    try {
+        const response = await axios.post(API_URL, data, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.status) {
+            localStorage.setItem('token', response.data.access);
+            localStorage.setItem('refresh_token', response.data.refresh);
+            toast('Login Successful.', {style: {backgroundColor: 'green', color: 'white'}})
+            return response
+        }
+    } catch (error) {
+        toast('Login Not Successful.', {style: {backgroundColor: 'red', color: 'white'}})
+        console.error("Login failed:", error.response?.data || error.message);
     }
-    return response.data;
 };
 
 const logout = () => {
@@ -25,16 +37,18 @@ const getAccessToken = () => {
 const refreshAccessToken = async () => {
     const refreshToken = localStorage.getItem('refresh_token');
     if (refreshToken) {
-        const response = await axios.post('http://localhost:8000/api/token/refresh/', { refresh: refreshToken });
+        const response = await axios.post(`${process.env.REACT_APP_API_URL}/token/refresh/`, { refresh: refreshToken });
         localStorage.setItem('token', response.data.access);
         return response.data.access;
     }
     return null;
 };
 
-export default {
+const authService = {
     login,
     logout,
     getAccessToken,
     refreshAccessToken,
-};
+}
+
+export default authService;
