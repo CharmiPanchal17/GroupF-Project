fe864f879a8336feec2d8545b94d3ce011e2a10f
CharmiPanchal17 <charmipanchal17@gmail.com>
2025-05-23 23:18:06 +0300

    deploying backend

--- backend/AITS/settings.py
+++ backend/AITS/settings.py
@@ -90,23 +90,23 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Aits db',
-        'USER' : 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT':'5432',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'Aits db',
+#         'USER' : 'postgres',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost',
+#         'PORT':'5432',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 SIMPLE_JWT = {
